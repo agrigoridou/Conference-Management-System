1dd3426fcb73a3f42297ed42535ec15ad9836373
athanasiagrigoridou <athanasia.grd@gmail.com>
2024-07-16 14:24:26 +0300

    Initial commit

---
+++ src/main/java/com/conference/management/App.java
@@ -0,0 +1,13 @@
+package com.conference.management;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
